952b29ca68c3a049a032fea7f85943bef6f5b3ad
nonloso220 <lucagabossis@gmail.com>
2022-03-29 14:58:17 +0200

    kkkk

--- sitoPiazzaLoggia/Js/codice.js
+++ sitoPiazzaLoggia/Js/codice.js
@@ -1,3 +1,18 @@
+$( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+});
 
 
 
